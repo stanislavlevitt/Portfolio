8b78fbe9ac183e4327291a74d3cccd8bd2b25202
Stanislav Levitt <stanislavlevitt@yahoo.com>
2020-05-29 12:37:38 -0400

    Rendering Other Tech Skillset

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -159,6 +159,16 @@ const Skills = () => (
                 }
               }
             }
+            otherTech {
+              id
+              skill
+              logo {
+                title
+                image: resize(width: 200, quality: 100) {
+                  src
+                }
+              }
+            }
           }
         }
       `}
@@ -233,6 +243,25 @@ const Skills = () => (
               className="card-container"
             >
               {contentfulAbout.techTools.map((p, i) => (
+                <Fade bottom delay={i * 200} key={p.id}>
+                  <SingleSkill {...p} />
+                </Fade>
+          ))}
+            </CardContainer>
+          </div>
+          <hr color="#c60055" />
+          <div className="SingleSkillContainer">
+            <div className="SkillTitle">
+              <Title margin="1em"> Creative and Analytical Tools:</Title>
+            </div>
+            <CardContainer
+              minWidth="350px"
+              style={{
+          width: '80%',
+        }}
+              className="card-container"
+            >
+              {contentfulAbout.otherTech.map((p, i) => (
                 <Fade bottom delay={i * 200} key={p.id}>
                   <SingleSkill {...p} />
                 </Fade>
